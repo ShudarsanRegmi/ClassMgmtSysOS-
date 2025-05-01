55e40f24039cdd95e5dc652c0c870e573ba5ade6
Shudarsan Regmi <shudarsanregmi555@gmail.com>
2025-05-01 12:20:13 +0530

    User profile udpate feature was completed

--- backend-classmgmt/routes/userRoutes.js
+++ backend-classmgmt/routes/userRoutes.js
@@ -2,35 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 
+
 const {
-    getUserProfile,
-    updateUserRole,
-    registerOrUpdateUser,
+    completeProfile,
+    getCurrentUserProfile, 
+    updateUserRole, 
     getAllUsers,
+    check
 } = require('../controllers/userController');
 
-const verifyFirebaseToken = require('../middlewares/authMiddleware');
+
+const verifyToken = require('../middleware/authmiddleware'); // Todo: To confirm that this modularization works fine
+
+// const verifyToken = require('../middleware/authMiddleware');
 
 
-// @route   POST /api/users/register
-// @desc    Register or update user on first login
-router.post('/register', verifyFirebaseToken, registerOrUpdateUser);
+router.get('/check', verifyToken, check);
+router.post("/complete-profile", verifyToken, completeProfile);
 
-
-// @route   GET /api/users/profile
-// @desc    Get current logged-in user's profile
-router.get('/profile', verifyFirebaseToken, getUserProfile);
-
-
-// @route   PUT /api/users/role
-// @desc    Update a user's role (Admin/CR/CA etc.)
-// @access  Restricted to Admin or Super Admin
-router.put('/role', verifyFirebaseToken, updateUserRole);
-
-
-// @route   PUT /api/users/role
-// @desc    Update a user's role (Admin/CR/CA etc.)
-// @access  Restricted to Admin or Super Admin
-router.put('/role', verifyFirebaseToken, updateUserRole);
 
 module.exports = router;
