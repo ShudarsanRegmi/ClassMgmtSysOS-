cb70f2753a1db0f9f1ae34873ea837d1cd46ea93
Shudarsan Regmi <shudarsanregmi555@gmail.com>
2025-06-08 09:50:16 +0530

    fixed: broken facutly assignent due to removal of faculty routes during cleanup

--- backend-classmgmt/app.js
+++ backend-classmgmt/app.js
@@ -8,6 +8,7 @@ const crRoutes = require('./routes/crRoutes');
 const classRoutes = require('./routes/classRoutes');
 const semesterRoutes = require('./routes/semesterRoutes');
 const courseRoutes = require('./routes/courseRoutes');
+const facultyRoutes = require('./routes/facultyRoutes');
 const settingsRoutes = require('./routes/systemSettingsRoutes');
 const assignmentRoutes = require('./routes/courseAssignment');
 const noticeRoutes = require('./routes/noticeRoutes');
@@ -51,6 +52,8 @@ app.use('/api/courses', courseRoutes);
 app.use('/api/sem', semesterRoutes);
 app.use('/api/notices', noticeRoutes);  
 app.use('/api/materials', courseMaterialRoutes);
+
+app.use('/api/faculty', facultyRoutes);
 app.use('/api/assignments/', assignmentRoutes);
 app.use('/api/admin/settings', settingsRoutes);
 app.use('/api/events', eventRoutes);
